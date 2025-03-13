6097413bb12802c0d2ad88fc040022a7ab92be4c
miikavonbell <miika.vonbell@outlook.com>
2025-03-13 14:14:00 +0200

    simplify

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import { ReititinClient } from '@reititin/client';
 import ollama from 'ollama';
-// Your Agent's ID
-const AGENT_ID = process.env.AGENT_ID || "default-agent-id";
-const OLLAMA_MODEL = process.env.OLLAMA_MODEL || "qwen2.5:3b";
+
 const processMessage = async (data) => {
-  const { message } = await ollama.chat({ model: OLLAMA_MODEL, messages: data });
+  const { message } = await ollama.chat({ model: process.env.OLLAMA_MODEL, messages: data });
   return message;
 };
-ReititinClient({ agentId: AGENT_ID, onMessage: processMessage });
+
+ReititinClient({ agentId: process.env.AGENT_ID, onMessage: processMessage });
